9141342aadee149783710a506e3882377a836622
aduarte98 <151936565+aduarte98@users.noreply.github.com>
2024-07-29 10:13:21 -0300

    Update footer.js

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -10,7 +10,7 @@ function Footer(){
             <img id="logo" alt='logo' src={Logo} />
             <span className='explicacao'>O CineFlix 4K é uma plataforma de site para assistir filmes e séries online gratuitamente! Nossa plataforma é um indexador automático. O CineFlix 4K não armazena filmes e/ou séries em nosso site, por isso está completamente dentro da lei. O CineFlix 4K indexa conteúdo(s) encontrado(s) na web automaticamente usando Robots e Inteligência Artificial (IA) & API TMDb. O uso do CineFlix é totalmente responsabilidade do usuário. A distribuição de filmes e/ou séries é da parte de plataformas como OpenLoad, Google Drive, entre outros. Qualquer violação de direitos autorais, entre em contato com o distribuidor.</span>
             <span>Todos os direitos reservados ©</span>
-            <span>Desenvolvidos por : Arthur Silva Duarte</span>
+            <span>Desenvolvidos por : Viserion </span>
 
         </footer>
     )
